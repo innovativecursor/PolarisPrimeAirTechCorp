6e6bbb9cb4df0c27c47eacaf0b347de14d13eb67
Aanchal Sharma <sharmaanchal8202@gmail.com>
2025-11-11 14:25:19 +0530

    changes in Purchase order API's

--- apps/pkg/supplierpo/config/config.go
+++ apps/pkg/supplierpo/config/config.go
@@ -1,10 +1,12 @@
 package config
 
+// AddSupplierPO is the request payload for creating a new Supplier Purchase Order
 type AddSupplierPO struct {
-	ProjectID     string             `json:"projectId" binding:"required"`
-	SupplierID    string             `json:"supplierId" binding:"required"`
-	CustomerPOIDs []string           `json:"customerPoIds,omitempty"` // multiple Customer POs linked
-	Items         []SupplierPOItemIn `json:"items" binding:"required"`
+	ProjectID     string             `json:"projectId" binding:"required"`  // Selected project
+	SupplierID    string             `json:"supplierId" binding:"required"` // Selected supplier
+	SOID          string             `json:"soId,omitempty"`                // Selected Sales Order
+	CustomerPOIDs []string           `json:"customerPoIds,omitempty"`       // Linked Customer POs if any (need to confirm from fe)
+	Items         []SupplierPOItemIn `json:"items" binding:"required"`      // Items to purchase
 }
 
 type UpdateSupplierPO struct {
@@ -12,13 +14,16 @@ type UpdateSupplierPO struct {
 	Items        []SupplierPOItemIn `json:"items" binding:"required"`
 }
 
+// SupplierPOItemIn represents an item being added/updated in a Supplier PO
+type SupplierPOItemIn struct {
+	Description string  `json:"description" binding:"required"` // Item description
+	Quantity    int     `json:"quantity" binding:"required"`    // Quantity
+	UOM         string  `json:"uom" binding:"required"`         // Unit of Measurement
+	Rate        float64 `json:"rate" binding:"required"`        // Rate per unit
+}
+
+// Not in use yet
 type ToggleSupplierPOStatus struct {
 	SupplierPOID string `json:"supplierPOId" binding:"required"`
 	Status       string `json:"status" binding:"required"`
-}
-
-type SupplierPOItemIn struct {
-	Description string  `json:"description" binding:"required"`
-	Quantity    int     `json:"quantity" binding:"required"`
-	Rate        float64 `json:"rate" binding:"required"`
 }
