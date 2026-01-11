dd2d63353a9ed8e5d25e4e0f0d682acd9c97a746
Shawn <shawn9920@gmail.com>
2026-01-12 02:46:37 +0530

    fixed CORS error

--- apps/config/config.go
+++ apps/config/config.go
@@ -24,6 +24,7 @@ type Config struct {
 			Password string `yaml:"password"`
 		} `yaml:"superAdmins"`
 	} `yaml:"seed"`
+	Endpoint string `yaml:"endpoint"`
 }
 
 func Env() (Config, error) {
--- apps/pkg/helper/cors/cors.go
+++ apps/pkg/helper/cors/cors.go
@@ -1,23 +1,58 @@
+// package cors
+
+// import (
+// 	"github.com/gin-gonic/gin"
+// )
+
+// // Helper function to use coors
+// func CORSMiddleware() gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		c.Writer.Header().Set("Content-Type", "application/json")
+// 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+// 		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+// 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+// 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max")
+// 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+//			if c.Request.Method == "OPTIONS" {
+//				c.AbortWithStatus(204)
+//			} else {
+//				c.Next()
+//			}
+//		}
+//	}
 package cors
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/innovativecursor/PolarisPrimeAirTechCorp/apps/config"
 )
 
-// Helper function to use coors
 func CORSMiddleware() gin.HandlerFunc {
+	// Pretty much for capturing the ENV endpoint
+	cfg, err := config.Env()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Content-Type", "application/json")
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		origin := c.Request.Header.Get("Origin")
+		// Gotta store the env value to our orignin and then set it in CORS's header
+		if origin == cfg.Endpoint {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		}
+
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
-		} else {
-			c.Next()
+			return
 		}
+
+		c.Next()
 	}
 }
